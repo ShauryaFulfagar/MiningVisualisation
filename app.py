bb30a6e218ef1852655c8f9b968c160fb54b113e
Shaurya Fulfagar <shaurya.fulfagar@gmail.com>
2024-03-02 10:55:15 +0530

    v2
    Graph plotted and color scheme changed

--- app.py
+++ app.py
@@ -15,43 +15,44 @@ india = gpd.read_file('india.shp')
 
 # Define a dictionary to map commodities to their real-life colors
 color_map = {
-    'Barite': '#777A86',
-    'Alumina': '#C0C085',
-    'Aluminum': '#7E7E80',
-    'Bauxite': '#AD6F69',
-    'Cement': '#8C7853',
-    'Borax': '#9AB973',
-    'Chromite': '#D9A441',
-    'Coal: bituminous': '#2C2B2B',
-    'Copper': '#B87333',
-    'Copper: metal': '#B87333',  # Adding the new commodity
-    'Diamond': '#B9F2FF',
-    'Gold': '#FFCC66',
-    'Ilmenite-rutile: ore': '#C3B091',
-    'Iron and steel: crude steel': '#43464B',
-    'Kyanite': '#6A5ACD',
-    'Lead: ore': '#8D4E85',
-    'Lead: primary': '#8D4E85',
-    'Lead: secondary': '#8D4E85',
-    'Lead-zinc: ore': '#8D4E85',
-    'Magnesite': '#CABBAC',
-    'Manganese: ore': '#AF9B60',
-    'Mica': '#BA9F93',
-    'Petroleum: refined': '#4B0082',
-    'Phosphate rock': '#2E86C1',
-    'Zinc': '#7C886A',
-    'Coal: lignite': '#964B00',
-    # Add more commodities and their colors here
+    'Barite': '#B3B3B3',                # Light Gray
+    'Alumina': '#F2F0A1',               # Pale Yellow
+    'Aluminum': '#A9A9A9',              # Dark Gray
+    'Bauxite': '#BC8F8F',               # Rosy Brown
+    'Cement': '#D2B48C',                # Tan
+    'Borax': '#E6E6E6',                 # Light Gray
+    'Chromite': '#CD853F',              # Peru
+    'Coal: bituminous': '#252525',      # Very Dark Gray
+    'Copper': '#B87333',                # Copper
+    'Copper: metal': '#B87333',         # Copper
+    'Diamond': '#B9F2FF',               # Diamond Blue
+    'Gold': '#FFCC66',                  # Gold
+    'Ilmenite-rutile: ore': '#A1887F',  # Timberwolf
+    'Iron and steel: crude steel': '#43464B',  # Charcoal
+    'Kyanite': '#6A5ACD',               # Slate Blue
+    'Lead: ore': '#525252',             # Dark Gray
+    'Lead: primary': '#525252',         # Dark Gray
+    'Lead: secondary': '#525252',       # Dark Gray
+    'Lead-zinc: ore': '#525252',        # Dark Gray
+    'Magnesite': '#CACACA',             # Silver
+    'Manganese: ore': '#AA9D5A',        # Olive Green
+    'Mica': '#A0522D',                  # Sienna
+    'Petroleum: refined': '#800080',    # Purple
+    'Phosphate rock': '#5DADE2',        # Sky Blue
+    'Zinc': '#789048',                  # Moss Green
+    'Coal: lignite': '#8B4513',         # Saddle Brown
 }
 
 
 # Plot the data on the static map
-fig, ax = plt.subplots(figsize=(10, 10))  # Increase the size of the figure
+fig, ax = plt.subplots(figsize=(15, 10))  # Increase the size of the figure
 plt.title("Map of Mineral and Metal Commodities in India")
 india.plot(ax=ax, color='white', edgecolor='black')
 for commodity, color in color_map.items():
-    gdf[gdf['commodity'] == commodity].plot(ax=ax, markersize=20, color=color, marker='o', label=commodity)
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')  # Move the legend outside the plot
+    gdf[gdf['commodity'] == commodity].plot(
+        ax=ax, markersize=20, color=color, marker='o', label=commodity)
+# Move the legend outside the plot
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 
 # Save the figure as an image file
 plt.savefig('Output_MapOfCommodities.png')
@@ -60,10 +61,11 @@ plt.savefig('Output_MapOfCommodities.png')
 commodity_counts = gdf['commodity'].value_counts()
 
 # Create a new figure for the bar graph
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(12.5, 11))
 
 # Create a bar graph
-bars = plt.bar(commodity_counts.index, commodity_counts.values, color=[color_map.get(commodity, '#333333') for commodity in commodity_counts.index])
+bars = plt.barh(commodity_counts.index, commodity_counts.values, color=[
+    color_map.get(commodity, '#333333') for commodity in commodity_counts.index])
 
 # Add labels and title
 plt.xlabel('Commodity')
@@ -75,6 +77,3 @@ plt.xticks(rotation=90)
 
 # Save the figure as an image file
 plt.savefig('Output_BarGraphOfCommodities.png')
-
-# Show the plot
-plt.show()
